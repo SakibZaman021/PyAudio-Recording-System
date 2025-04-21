d80d321f071ea16ab7d5b6ea243d0bf1e32ede24
SakibZaman021 <sakibzaman169@gmail.com>
2025-04-21 14:38:25 +0600

    Recommitted updated singleton_user.py

--- singleton_user.py
+++ singleton_user.py
@@ -1,33 +1,3 @@
-# from datetime import datetime
-# from threading import Lock
-
-# class UserSingleton:
-#     _instance = None
-#     _lock = Lock()
-
-#     @staticmethod
-#     def getInstance(id=None, hospital=None, doc_id=None):
-#         with UserSingleton._lock:
-#             if UserSingleton._instance is None:
-#                 if id is None or hospital is None or doc_id is None:
-#                     raise ValueError("UserSingleton requires id, hospital, and doc_id for initialization")
-#                 UserSingleton(id, doc_id, hospital)
-#             return UserSingleton._instance
-    
-#     @staticmethod
-#     def delInstance():
-#         with UserSingleton._lock:
-#             UserSingleton._instance = None
-        
-#     def __init__(self, id, doc_id, hospital):
-#         if UserSingleton._instance is not None:
-#             raise Exception("Singleton class already exists")
-#         else:
-#             UserSingleton._instance = self
-#             self.userId = id
-#             self.doctorId = doc_id
-#             self.hospital = hospital
-#             self.start = datetime.now().strftime("%H:%M")
 from threading import Lock
 from datetime import datetime
 
@@ -43,15 +13,6 @@ class UserSingleton:
             self.userId = user_id
             self.hospital = hospital
             self.doctorId = doctor_id
-
-    # @staticmethod
-    # def getInstance(user_id_x, hospital_y, doctor_id_z):
-    #     print(user_id_x, hospital_y, doctor_id_z + "aaa")
-    #     with UserSingleton._lock:
-    #         if UserSingleton._instance is None:
-    #             UserSingleton._instance = UserSingleton(user_id_x, hospital_y, doctor_id_z)
-    #         return UserSingleton._instance
-    
     
     @classmethod
     def getInstance(cls, user_id=None, hospital=None, doctor_id=None):
